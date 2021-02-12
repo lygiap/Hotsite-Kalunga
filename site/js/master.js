b7658395e004499840dd0e657ea56e2a59aea3c0
Lygia P L <19375302+lygiap@users.noreply.github.com>
2021-02-11 23:12:54 -0300

    Removido código js do arquivo html, separando em uma pasta

---
+++ site/js/master.js
@@ -0,0 +1,52 @@
+// Função mouseOver
+
+function mouseOver0() {
+    var x = document.getElementsByClassName("bg-button");
+    x[0].style.visibility = "visible";
+
+    var x = document.getElementsByClassName("btn");
+    x[0].style.visibility = "visible";
+}
+
+function mouseOver1() {
+    var x = document.getElementsByClassName("bg-button");
+    x[1].style.visibility = "visible";
+
+    var x = document.getElementsByClassName("btn");
+    x[1].style.visibility = "visible";
+}
+
+function mouseOver2() {
+    var x = document.getElementsByClassName("bg-button");
+    x[2].style.visibility = "visible";
+
+    var x = document.getElementsByClassName("btn");
+    x[2].style.visibility = "visible";
+}
+
+
+// Função mouseOut
+
+function mouseOut0() {
+    var x = document.getElementsByClassName("bg-button");
+    x[0].style.visibility = "hidden";
+
+    var x = document.getElementsByClassName("btn");
+    x[0].style.visibility = "hidden";
+}
+
+function mouseOut1() {
+    var x = document.getElementsByClassName("bg-button");
+    x[1].style.visibility = "hidden";
+
+    var x = document.getElementsByClassName("btn");
+    x[1].style.visibility = "hidden";
+}
+
+function mouseOut2() {
+    var x = document.getElementsByClassName("bg-button");
+    x[2].style.visibility = "hidden";
+
+    var x = document.getElementsByClassName("btn");
+    x[2].style.visibility = "hidden";
+}
